293894f59b94cf68580f3198083061c1d0f97a08
AndreiSiahlo <50456403+AndreiSiahlo@users.noreply.github.com>
2022-03-24 14:44:57 +0300

    Add files via upload

---
+++ SketchAr020BlinkOnOff.ino
@@ -0,0 +1,16 @@
+// https://habr.com/ru/post/352806/
+int Led = 13; //  // declare and set the variable Led
+void setup(){
+    pinMode(Led, OUTPUT); // Set the variable Led to the OUTPUT
+}
+
+int TimeSlot=1000;
+
+void loop()
+{  int DurationOn=TimeSlot*0.5;
+   int DurationOff=TimeSlot-DurationOn;
+    digitalWrite(Led, HIGH); // the voltage is applied to the pin 13
+    delay(DurationOn); // wait 0.5 seconds
+    digitalWrite(Led, LOW); // no voltage is applied to the pin 13
+    delay(DurationOff); // wait 0.5 seconds
+  }
