2e06c717b9807dfb9e61971fd6d7ddfbe3937e41
AndreiSiahlo <50456403+AndreiSiahlo@users.noreply.github.com>
2022-03-31 11:22:13 +0300

    Add files via upload

--- SketchAr020BlinkOnOff.ino
+++ SketchAr020BlinkOnOff.ino
@@ -1,4 +1,5 @@
 // https://habr.com/ru/post/352806/
+// The sketch makes an LED on the Arduino plate light up and light off periodically
 int Led = 13; //  // declare and set the variable Led
 void setup(){
     pinMode(Led, OUTPUT); // Set the variable Led to the OUTPUT
