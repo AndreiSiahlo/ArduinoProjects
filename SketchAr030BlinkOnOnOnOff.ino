59a926d7977aa41e44e1e09a1219153f41193b1c
AndreiSiahlo <50456403+AndreiSiahlo@users.noreply.github.com>
2022-03-31 11:55:56 +0300

    Add files via upload

--- SketchAr030BlinkOnOnOnOff.ino
+++ SketchAr030BlinkOnOnOnOff.ino
@@ -1,5 +1,9 @@
 // SketchAr030BlinkOnOnOnOff
 // https://habr.com/ru/post/352806/
+// The LED on the Arduino plate is switched on for 3 sec.
+// And the voltage of 5V between the GND port and the Pin13 is set.
+// Thereafter the LED turns out for the short time. 
+// At this short time no voltage is set to Pin13.
 // This sketch could be used to check the ports voltage
 int Led = 13; // declare and set the variable Led
 void setup(){
